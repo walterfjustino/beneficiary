a9c10f425c8b4225734c815485dae9c41c2dd802
walterfjustino <walterfjustino@gmail.com>
2025-06-15 17:21:42 -0300

    [master][add structure project on github]

---
+++ application/src/main/java/com/management/beneficiary/BeneficiaryApplication.java
@@ -0,0 +1,15 @@
+package com.management.beneficiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan("com.management.beneficiary")
+@SpringBootApplication
+public class BeneficiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeneficiaryApplication.class, args);
+	}
+
+}
