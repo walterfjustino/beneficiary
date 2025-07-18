143a79ea32acae5f35cef41b65ca18d1164aeb01
walterfjustino <walterfjustino@gmail.com>
2025-07-17 21:22:42 -0300

    [master][adjustment directory in @ComponentScan]

--- application/src/main/java/com/management/beneficiary/BeneficiaryApplication.java
+++ application/src/main/java/com/management/beneficiary/BeneficiaryApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan("com.management.beneficiary")
+@ComponentScan("com.management.beneficiary.*")
 @SpringBootApplication
 public class BeneficiaryApplication {
 
